e453ebc92658d2cf0a5c016179cb01264c971622
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-12-03 20:50:12 +0200

    Add extension method System.IComparable1.IsInRange

--- Source/Padutronics.Extensions.System/ComparableExtensions.cs
+++ Source/Padutronics.Extensions.System/ComparableExtensions.cs
@@ -22,6 +22,21 @@ public static class ComparableExtensions
         return @this.CompareTo(lowerBound) >= 0;
     }
 
+    public static bool IsInRange<T>(this T @this, T inclusiveLowerBound, T inclusiveUpperBound)
+        where T : IComparable<T>
+    {
+        return IsInRange(@this, inclusiveLowerBound, inclusiveUpperBound, isLowerBoundIncluded: true, isUpperBoundIncluded: true);
+    }
+
+    public static bool IsInRange<T>(this T @this, T lowerBound, T upperBound, bool isLowerBoundIncluded, bool isUpperBoundIncluded)
+        where T : IComparable<T>
+    {
+        bool isLowerBoundValid = isLowerBoundIncluded ? @this.CompareTo(lowerBound) >= 0 : @this.CompareTo(lowerBound) > 0;
+        bool isUpperBoundValid = isUpperBoundIncluded ? @this.CompareTo(upperBound) <= 0 : @this.CompareTo(upperBound) < 0;
+
+        return isLowerBoundValid && isUpperBoundValid;
+    }
+
     public static bool IsLessThan<T>(this T @this, T upperBound)
         where T : IComparable<T>
     {
